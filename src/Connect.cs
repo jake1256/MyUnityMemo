54bad5da7eefc34d251b7371c8a33abfcb6f1131
Jake <rock.zeke@gmail.com>
2014-06-23 22:46:27 +0900

    add my memo

---
+++ src/Connect.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using MiniJSON;
+
+/**
+ * 通信はこんな感じ
+ */
+public class Connect : MonoBehaviour{
+
+	private string URL = "";
+
+	public void connect(){
+		StartCoroutine(getConnect (URL));
+	}
+
+	private IEnumerator getConnect(string url){
+		WWW www = new WWW(url);
+		yield return www;
+		if(www.error != null){
+			Debug.Log("connect get error!!");
+		}else{
+			Debug.Log("success!! connect get");
+			parseJSON(www.text);
+		}
+	}
+
+	private IEnumerator postConnect(string url){
+		WWWForm wwwForm = new WWWForm();
+		wwwForm.AddField("param" , 1);
+		WWW www = new WWW(url);
+
+		yield return www;
+
+		if(www.error != null){
+			Debug.Log("connect get error!!");
+		}else{
+			Debug.Log("success!! connect post");
+
+			parseJSON(www.text);
+		}
+
+	}
+
+	private void parseJSON(string responseData){
+		// webサーバからの内容を文字列変数に格納
+		string json = responseData;
+		// 以降JSONのパースは同じ
+		IList familyList = (IList)Json.Deserialize(json);
+
+		foreach(IDictionary person in familyList){
+			string name = (string)person["name"];
+			Debug.Log("name:"+name);
+
+			long age = (long)person["age"];
+			Debug.Log("age:"+age);
+
+			IList hobbes = (IList)person["hobby"];
+			foreach(string hobby in hobbes){
+				Debug.Log("hobby:"+hobby);
+			}
+		}
+	}
+}
---
+++ src/CreateStage.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ /**
+  * ステージを作成
+  */
+public class CreateStage : MonoBehaviour {
+
+	private Connect connect;
+
+	public GameObject cube;
+	public GameObject wall;
+	public int countW;
+	public int countH;
+	public int startX;
+	public int startY;
+
+	// Use this for initialization
+	void Start () {
+		connect = new Connect();
+
+		int x;
+		int y;
+		int z;
+
+		int value = 2;
+
+		Transform transform = this.cube.transform;
+
+		for(int i = 0 ; i < (countW * value) ; i++){
+			x = startX + i;
+			y = 1;
+			for(int j = 0 ; j < (countH * value) ; j++){
+				z = startY + j;
+				if(i > 0 && i < (countW * value) - 1){
+					if(j == 0 || j == (countH * value) - 1){
+						Instantiate(this.wall , new Vector3(x , y , z) , transform.rotation);
+					}
+				}else{
+					Instantiate(this.wall , new Vector3(x , y , z) , transform.rotation);
+				}
+			}
+		}
+
+		for(int i = 0 ; i < countW ; i++){
+			x = startX + (i * 2);
+			y = 1;
+			for(int j = 0 ; j < countH ; j++){
+				z = startY + (j * 2);
+
+				Instantiate(this.cube , new Vector3(x , y , z) , transform.rotation);
+			}
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
